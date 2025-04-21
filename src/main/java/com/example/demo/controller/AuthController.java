5e98c67c064eb81db1b8e1499ad36596ff502b5e
Olivier Lafleur <lafleuro@uqtr.ca>
2025-04-20 20:32:20 -0400

    dd

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+    // test
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody AuthDTO authDTO) {
         try {
